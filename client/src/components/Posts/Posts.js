7a343b48fe9f8b81d2b2b69d1b046986687f1d23
vilmosan <william.irons1992@gmail.com>
2021-09-11 22:46:12 +0200

    Post client side rendering structure added with styling.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
@@ -10,11 +11,15 @@ const Posts = () => {
 	console.log(posts);
 
 	return (
-		<React.Fragment>
-			<h1>POSTS</h1>
-			<Post></Post>
-			<Post></Post>
-		</React.Fragment>
+		!posts.length ? <CircularProgress /> : (
+			<Grid className={classes.container} container alignItems={"stretch"} spacing={3}>
+				{posts.map((post) => (
+					<Grid key={post._id} item xs={12} sm={6}>
+						<Post key={post._id} post={post}/>
+					</Grid>
+				))}
+			</Grid>
+		)
 	);
 };
 

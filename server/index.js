a92c45cc691def4157868e9dd5c679b938c6834a
Vilmos Vass <vilmosvass@ancora.health>
2022-01-16 12:52:22 +0100

    Searching posts by title or tags.

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json({ limit: '30mb' }));
-app.use(express.urlencoded({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: false }));
 app.use(cors()); // Must be called earlier than routes declaration.
 
 app.use('/posts', postRoutes);
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import { getPosts, getPostsBySearch, createPost, updatePost, deletePost, likePost } from "../controllers/posts.js";
 
 import auth from '../middleware/auth.js';
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 // http://localhost:5000/posts/ because we added a prefix of posts for all routes in here in the index.js file.
 router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
